8d1fe1e0286b9db63c91ddd1368851e25c25d4b8
Shanfeng Lu <lsf@msn.cn>
2019-02-21 23:05:33 +0800

    改错
    第一次的eval前面多了一个括号，导致语法错误。
    起步阶段，不够小心。
    
    另外还有一个小错误是将乘除搞反了，算数学错误。

--- 练习与测试/rmb_usd_Convert.py
+++ 练习与测试/rmb_usd_Convert.py
@@ -1,9 +1,9 @@
 Dollor = input()
 if Dollor[0:3] in ['rmb','RMB']:
-    USD = (eval(Dollor[3:])*6.78
+    USD = eval(Dollor[3:])/6.78
     print ("USD{:.2F}".format(USD)) #这么会报错，但是错在哪里呢？
 elif Dollor[0:3] in ['USD','usd']:
-    RMB = (eval(Dollor[3:]))/6.78
+    RMB = (eval(Dollor[3:]))*6.78
     print("RMB{:.2f}".format(RMB))
 else:
     print("您的输入格式有误")
